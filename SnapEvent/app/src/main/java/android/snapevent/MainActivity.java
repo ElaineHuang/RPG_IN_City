a065c1fba6bddb8507431e9307d574b953b52ace
Elaine Huang <starbrave1313677@gmail.com>
2015-08-22 08:40:21 +0800

    new

---
+++ SnapEvent/app/src/main/java/android/snapevent/MainActivity.java
@@ -0,0 +1,49 @@
+package android.snapevent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
+import android.view.View;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hsuan-ju on 2015/8/10.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+
+
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayShowTitleEnabled(false);
+        //ab.setDisplayHomeAsUpEnabled(true);
+        List<Fragment> fl=new ArrayList<Fragment>();
+        fl.add(new FirstPage());
+        fl.add(new ListPage());
+        fl.add(new PhotoPage());
+      ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            viewPager.setAdapter(new MainPageAdapter(getSupportFragmentManager(), fl , MainActivity.this));
+        }
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+}
---
+++ SnapEvent/app/src/main/java/android/snapevent/MainPageAdapter.java
@@ -0,0 +1,44 @@
+package android.snapevent;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+
+import java.util.List;
+
+/**
+ * Created by ser on 2015/8/12.
+ */
+public class MainPageAdapter extends FragmentStatePagerAdapter {
+
+    private String tabTitles[] = new String[] { "攻略進度","任務清單", "照片上傳" };
+
+    private Context context;
+    List<Fragment> fragments;
+
+    public MainPageAdapter(FragmentManager fm , List<Fragment> f,Context context) {
+        super(fm);
+        this.context=context;
+        fragments=f;
+
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+    @Override
+    public Fragment getItem(int position) {
+       return fragments.get(position);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
